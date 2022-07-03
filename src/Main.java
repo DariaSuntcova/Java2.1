90329f1e1c11581f5c07ac247dec015b20357aa6
Suntcova Daria <batareyka23@gmail.com>
2022-07-03 20:59:35 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 12_943;
+        int forOneBonus = 20;
+        int mili = ticketPrice / forOneBonus;
+        System.out.print(mili);
+    }
+}
